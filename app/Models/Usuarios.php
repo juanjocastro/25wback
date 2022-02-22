beb18283b74a2551d895d753fd3e75a8aad9949d
juanjocastro <juanjocastro@gmail.com>
2022-02-22 18:28:15 -0300

    prueba1000

--- app/Models/Usuarios.php
+++ app/Models/Usuarios.php
@@ -9,11 +9,11 @@ class Usuarios extends Model
 {
     use HasFactory;
 
-    protected $table = 'usuarios';
+    protected $table = 'datos_formulario';
     protected $fillable = [
-        'name',
+        'nombre',
         'email',
-        'phone',
-        'message'       
+        'telefono',
+        'mensaje'       
     ];
 }
